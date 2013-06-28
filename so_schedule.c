aebf7c98d200471decb400c629d687810e4fd6c0
= <=>
2013-06-28 17:02:25 -0300

    comeco do so_schedule

--- so_schedule.c
+++ so_schedule.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <time.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -30,8 +31,17 @@ typedef struct info{
 
 }INFO_T;
 
+typedef struct requisicao{
+	int tempo_restante;
+	int num_proc;
+}REQUISICAO_T;
+
+
+int max_proc,proc_livres;
 int idsem;
 struct sembuf operacao[1];
+PROCESSO_T *pshm;
+INFO_T *p2shm;
 
 int p_sem()
 {
@@ -50,8 +60,7 @@ int v_sem()
        printf("erro no p=%d\n", errno);
 }
 
-
-int std2sec(char *std_time){
+int str2sec(char *std_time){
 
 	char hr[3],min[3],sec[3];
 	int time;
@@ -70,28 +79,62 @@ int std2sec(char *std_time){
 }
 
 
+void scheduler(){
+	
+	int i, nreq_escolhido=0;
+	int max_time,start_time,current_time;
+	char time_str[9];
+	time_t time_now;
+	REQUISICAO_T req;
+
+
+	p_sem();
+	
+		if(proc_livres < max_proc){
+			for(i=0;i<NUM_TAB;i++){
+				if((pshm[i].nreq != 0)&&(pshm[i].status == PENDING)){
+				
+					max_time = str2sec(pshm[i].max_time);
+
+					strftime(time_str,9,"%T",localtime(&pshm[i].start_time));
+					start_time = str2sec(time_str);
+
+					time_now = time(NULL);
+					strftime(time_str,9,"%T",localtime(&time_now));
+					current_time = str2sec(time_str);
+
+					req.tempo_restante = max_time - (current_time - start_time);
+
+
+					printf("Start: %d\nCurrent: %d\nMax: %d\nDif: %d\n\n\n",start_time,current_time,max_time,req.tempo_restante);
+
+				}
+			}
+
+		}	
+
+	v_sem();
+}
+
+
 
 
 
 int main(int argc,char* argv[]){
 
+	int idshm,id2shm,idsem;
 
-	int max_proc,processos_running;
-	int idshm, id2shm;
-	INFO_T *p2shm;
-	PROCESSO_T *pshm,*paux;
-	struct tm * timeinfo;
-	char start_time[50];
-	int i;
 
+	signal(SIGALRM,scheduler);
 
 	if(argc == 1){
 		printf("Numero maximo de processos nao informado\n");
-		exit(-1);
+		exit(1);
 	}
 
 	max_proc = atoi(argv[1]);
-	processos_running = 0;
+	proc_livres = max_proc;
+
 
 	/*da um shmget na mem compartilhada*/	
 	if ((idshm = shmget(90108094, NUM_TAB*sizeof(struct processo), IPC_CREAT|0x1ff)) < 0){
@@ -111,15 +154,13 @@ int main(int argc,char* argv[]){
 	    printf("erro na criacao da memoria compartilhada\n");
 	    exit(1);
 	}
-	
+
 	/*da um attach na mem compartilhada de bloqueio para ver se pode escrever ou nao*/
 	p2shm = (INFO_T *) shmat(id2shm, (char *)0, 0);
 	if (p2shm == (INFO_T *)-1){
         printf("erro no attach\n");
         exit(1);
     }
-
-	
 
 	/*da um semget em semaforo para cria-lo*/
 	if ((idsem = semget(90015266, 1, IPC_CREAT|0x1ff)) < 0){
@@ -128,35 +169,11 @@ int main(int argc,char* argv[]){
 	}
 
 
-    /*da um semget em semaforo para ter acesso */
-	if ((idsem = semget(90015266, 1, IPC_CREAT|0x1ff)) < 0){
-	     printf("erro na criacao do semaforo\n");
-	     exit(1);
+	while(1){
+		alarm(1);
+		sleep(2);
 	}
-
-	p_sem();
-
-	for (i = 0; i < NUM_TAB; i++)
-	{
-
-		timeinfo = localtime(&pshm[i].start_time);
-		strftime(start_time,50,"%X",timeinfo);
-
-		printf("%s\n",start_time);
-	}
-
-	v_sem();
-
-
-
-
-
-
-
-
-
-
-
 
 	return 0;
+
 }
